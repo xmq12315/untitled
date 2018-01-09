40ae22daa80dfec630722c274bc1a1813ba6f230
xmq12315 <30655484+xmq12315@users.noreply.github.com>
2018-01-09 18:36:28 +0800

    1

--- untitled1.py
+++ untitled1.py
@@ -6,20 +6,16 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.abspath(os.path.dirname(__file__))
         upload_path = os.path.join(basepath, 'static/uploads')
-        f.save(upload_path, secure_filename(f.filemame))
-        return redirect(url_for('hello_world'))
+        f.save(upload_path, secure_filename(f.filename))
+        return redirect(url_for('upload'))
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
